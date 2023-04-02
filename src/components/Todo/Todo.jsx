c01aadd96710f896fd0d99f1f07d24b79fcfa888
namdaeun <nde40345@gmail.com>
2023-04-02 11:44:32 +0900

    checkbox, onDelete

---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BsFillTrashFill } from 'react-icons/bs';
+
+export default function Todo({ todo, onUpdate, onDelete }) {
+    const { text, status } = todo;
+    const handleChange = (e) => {
+        const status = e.target.checked ? 'completed' : 'active';
+        onUpdate({ ...todo, status});
+    };
+    const handleDelete = () => { onDelete(todo) };
+    return (
+        <li>
+            <input 
+                type='checkbox' 
+                id = 'checkbox' 
+                checked={status === 'completed'}
+                onChange = {handleChange}
+            />
+            <label htmlFor='checkbox'>{text}</label>
+            <button onClick={handleDelete}>
+                <BsFillTrashFill />
+            </button>
+        </li>
+    );
+}
+
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,25 +1,33 @@
 import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
+import Todo from '../Todo/Todo';
 
 
 export default function TodoList() {
     const [todos, setTodos] = useState([
-        {id: '123', text: '프언론 과제', status: 'active'},
-        {id: '124', text: '프언론 복습', status: 'active'}
+        { id: '123', text: '프언론 과제', status: 'active' },
+        { id: '124', text: '프언론 복습', status: 'active' }
     ]);
-    const handleAdd= (todo) => {
-        // 새로운 투두를 todos에 update
-        console.log(todo);
+    const handleAdd = (todo) => {// 새로운 투두를 todos에 update
         setTodos([...todos, todo]);
     };
-     return (
+    const handleUpdate = (updated) => {// 기존의 id와 update되는 id가 같으면 update된 걸 씀
+        setTodos(todos.map((t) => t.id === updated.id ? updated : t));
+    };
+    const handleDelete = (deleted) => {// 삭제하고자하는 id가 아닌 경우만 filter해서 새로운 배열 만듦
+        setTodos(todos.filter(t => t.id !== deleted.id));
+    };
+    return (
         <section>
             <ul>
                 {
-                    todos.map((item) => (<li key={item.id}>{item.text}</li>
+                    todos.map((item) => (<Todo 
+                        key={item.id} todo={item} 
+                        onUpdate={handleUpdate} onDelete={handleDelete} 
+                        />
                 ))}
             </ul>
-            <AddTodo onAdd={handleAdd}/>
+            <AddTodo onAdd={handleAdd} />
         </section>
     );
 }
