ce99bb9f56adf1e079fd76dd2118228f5ab38c65
namdaeun <nde40345@gmail.com>
2023-04-08 21:11:54 +0900

    uniqueKey

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styles from './AddTodo.module.css';
 // todo가 새로 입력되면 TodoList에 전달해줌
 
-export default function AddTodo({ onAdd }) {
+export default function AddTodo({ id, onAdd }) {
     const [text, setText] = useState('');
     const handleChange = (e) => setText(e.target.value);
     const handleSubmit = (e) => {
@@ -10,7 +10,7 @@ export default function AddTodo({ onAdd }) {
         if (text.trim().length === 0){ // trim - 텍스트의 여백 없앰
             return; // 공백이 입력된 경우 리스트에 추가하지 않음
         }
-        onAdd({id: '고유한 값', text, status: 'active'});
+        onAdd({id: {id}, text, status: 'active'});
         setText(''); // 문자열 초기화
     };
     return (
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 import styles from './TodoList.module.css';
 
 export default function TodoList({ filter }) {
-    const [todos, setTodos] = useState([
-        { id: '123', text: '프언론 과제', status: 'active' },
-        { id: '124', text: '프언론 복습', status: 'active' }
-    ]);
+    //const [todos, setTodos] = useState(readTodosFromLocalStorage()); // 이렇게 하면 불필요하게 많이 호출함
+    const [todos, setTodos] = useState(() => readTodosFromLocalStorage()); // 따라서 콜백함수 사용
+
     const handleAdd = (todo) => setTodos([...todos, todo]); // 새로운 투두를 todos에 update
     const handleUpdate = (updated) =>  // 기존의 id와 update되는 id가 같으면 update된 걸 씀
         setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
@@ -17,15 +16,15 @@ export default function TodoList({ filter }) {
     useEffect(() => {
         localStorage.setItem('todos', JSON.stringify(todos));
         // todos라는 key에 todos를 JSON형태로 변경한 것을 저장
-    }, [todos]) // todos가 변경이 될 때마다
+    }, [todos]) // todos가 변경이 될 때마다 실행
 
     const filtered = getFilteredItems(todos, filter);
     return (
         <section className={styles.container}>
             <ul className={styles.list}>
-                {filtered.map((item) => (
+                {filtered.map((item, idx) => (
                     <Todo  // 필터링된 아이템들만 보여지도록
-                        key={item.id} 
+                        key={idx} // key값을 고유하게 만듦
                         todo={item} 
                         onUpdate={handleUpdate} 
                         onDelete={handleDelete} 
@@ -36,9 +35,16 @@ export default function TodoList({ filter }) {
         </section>
     );
 }
+function readTodosFromLocalStorage() {
+
+    const todos = localStorage.getItem('todos'); // todos값 가져와서 todos에 저장
+    return todos ? JSON.parse(todos) : [];   
+    // todo값이 있으면 JSON으로 parse하여 변환한 배열을 리턴, 없으면 공백 리턴
+}
+
 function getFilteredItems(todos, filter) {
-    if(filter === 'all' ) {
-        return todos;
+    if (filter === 'all') {
+      return todos;
     }
     return todos.filter((todo) => todo.status === filter);
-}
+  }
