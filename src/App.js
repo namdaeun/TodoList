a9b5fe05ffa19a97fdbafd8c5dba789eaf27241d
namdaeun <nde40345@gmail.com>
2023-11-13 13:27:56 +0900

    docs: 빈칸 추가

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { DarkModeProvider } from './context/DarkModeContext';
 const filters = ['all', 'active', 'completed'];
 function App() {
   const [filter, setFilter] = useState(filters[0]);
-  return (
+  return ( 
     <DarkModeProvider>
       <Header filters={filters} filter={filter} onFilterChange={setFilter} />
       <TodoList filter={filter} />
